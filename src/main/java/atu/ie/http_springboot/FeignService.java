44e23006b6710813b03ca0c77a942721081fdf1e
Mikaela <G00401392@atu.ie>
2023-10-18 15:36:14 +0100

    Added code to FeignService

--- src/main/java/atu/ie/http_springboot/FeignService.java
+++ src/main/java/atu/ie/http_springboot/FeignService.java
@@ -1,5 +1,22 @@
 package atu.ie.http_springboot;
 
-public class FeignService {
+import org.springframework.stereotype.Service;
 
+@Service
+public class FeignService {
+    private final TodoClient todoClient;
+
+    public FeignService(TodoClient todoClient) {
+        this.todoClient = todoClient;
+    }
+    public TodoResponse fetchData() {
+        TodoResponse td = todoClient.fetchData();
+        System.out.println(td);
+        try {
+            Thread.sleep(2000);
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        return td;
+    }
 }
