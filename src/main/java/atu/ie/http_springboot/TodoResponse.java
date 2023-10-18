81aef3780576e3975bb00ba12b9c263e4aa6f8b0
Mikaela <G00401392@atu.ie>
2023-10-18 15:15:51 +0100

    Added code to TodoResponse

--- src/main/java/atu/ie/http_springboot/TodoResponse.java
+++ src/main/java/atu/ie/http_springboot/TodoResponse.java
@@ -1,4 +1,20 @@
 package atu.ie.http_springboot;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
 public class TodoResponse {
+    @JsonProperty("userId")
+    private int userId;
+
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("completed")
+    private boolean completed;
+
 }
