8b374862f2682ef92a0322c0fa04cd8bba72a1b4
Mikaela <G00401392@atu.ie>
2023-10-18 14:35:36 +0100

    Created new class BlockingController

--- src/main/java/atu/ie/http_springboot/HttpSpringBootApplication.java
+++ src/main/java/atu/ie/http_springboot/HttpSpringBootApplication.java
@@ -2,8 +2,10 @@ package atu.ie.http_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HttpSpringBootApplication {
 
     public static void main(String[] args) {
