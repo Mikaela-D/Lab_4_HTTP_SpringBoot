45a4ca5b32c2635ecc5c00dbf4c0e05e62bbe6f1
Mikaela <G00401392@atu.ie>
2023-10-18 14:29:16 +0100

    Created new project HTTP_SpringBoot

---
+++ src/main/java/atu/ie/http_springboot/HttpSpringBootApplication.java
@@ -0,0 +1,13 @@
+package atu.ie.http_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/atu/ie/http_springboot/HttpSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.http_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
