8e2752a284875e2c7b87b2b2d0f1e2c8770985cf
Mikaela <G00401392@atu.ie>
2023-10-18 14:44:04 +0100

    Added code to BlockingController

--- src/main/java/atu/ie/http_springboot/HttpSpringBootApplication.java
+++ src/main/java/atu/ie/http_springboot/HttpSpringBootApplication.java
@@ -3,6 +3,8 @@ package atu.ie.http_springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -11,5 +13,8 @@ public class HttpSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(HttpSpringBootApplication.class, args);
     }
-
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
